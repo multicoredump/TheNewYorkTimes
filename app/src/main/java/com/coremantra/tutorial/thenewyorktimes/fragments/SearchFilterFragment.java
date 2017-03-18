1756da23462a5ee3f65cc2d0859bc96ca3dd3571
Radhika Karandikar <radhikak@lab126.com>
2017-03-17 22:16:26 -0700

    v0.8.7: Added Spinner widget for Sort Order

--- app/src/main/java/com/coremantra/tutorial/thenewyorktimes/fragments/SearchFilterFragment.java
+++ app/src/main/java/com/coremantra/tutorial/thenewyorktimes/fragments/SearchFilterFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 
 import com.coremantra.tutorial.thenewyorktimes.R;
 import com.coremantra.tutorial.thenewyorktimes.models.SearchFilters;
@@ -34,6 +33,7 @@ import butterknife.ButterKnife;
 public class SearchFilterFragment extends DialogFragment {
 
     private static final String ARG_FILTERS = "filters";
+    private static final String TAG = "NY: " + SearchFilterFragment.class.getName();
 
     private SearchFilters filters;
 
@@ -60,11 +60,10 @@ public class SearchFilterFragment extends DialogFragment {
     @BindView(R.id.cbTech)
     CheckBox cbTech;
 
-    @BindView(R.id.radioOldest)
-    RadioButton radioOldest;
+    @BindView(R.id.spSort)
+    Spinner spSort;
 
-    @BindView(R.id.radioNewest)
-    RadioButton radioNewest;
+
 
     View.OnClickListener saveClickListener = new View.OnClickListener() {
         @Override
@@ -73,7 +72,7 @@ public class SearchFilterFragment extends DialogFragment {
            if (mListener != null) {
 
                filters.update(etBeginDate.getText().toString(),
-                       radioOldest.isChecked(),
+                       spSort.getSelectedItem().toString().toLowerCase().equals(SearchFilters.SORT_OLDEST.toLowerCase()),
                        cbFood.isChecked(), cbFashion.isChecked(), cbDining.isChecked(), cbTravel.isChecked(), cbTech.isChecked());
 
                mListener.onFinishDialog(filters);
@@ -113,12 +112,16 @@ public class SearchFilterFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.fragment_search_filter, container, false);
         ButterKnife.bind(this, view);
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        String sortOrder[] = getActivity().getResources().getStringArray(R.array.sort_order);
 
         if (filters.getBeginDateString() != null) {
             etBeginDate.setText(filters.getBeginDateString());
@@ -129,8 +132,13 @@ public class SearchFilterFragment extends DialogFragment {
         cbTravel.setChecked(filters.isTravel());
         cbTech.setChecked(filters.isTech());
 
-        radioOldest.setChecked(filters.isSortOldest());
-        radioNewest.setChecked(!filters.isSortOldest());
+        Log.d(TAG, "Sort order is " + filters.getSortOrder());
+        for (int i = 0; i < sortOrder.length; i++) {
+            if (sortOrder[i].toLowerCase().equals(filters.getSortOrder())) {
+                spSort.setSelection(i);
+                break;
+            }
+        }
 
         btSave.setOnClickListener(saveClickListener);
     }
@@ -150,19 +158,5 @@ public class SearchFilterFragment extends DialogFragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-
-
-    public void setSpinnerToValue(Spinner spinner, String value) {
-        int index = 0;
-        SpinnerAdapter adapter = spinner.getAdapter();
-        for (int i = 0; i < adapter.getCount(); i++) {
-            if (adapter.getItem(i).equals(value)) {
-                index = i;
-                break; // terminate loop
-            }
-        }
-        spinner.setSelection(index);
     }
 }
